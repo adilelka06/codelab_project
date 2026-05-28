48144de69e99f9be98aa038a254c90f1ade68db8
adilelka06 <adilelka06@hotmail.com>
2026-05-28 02:08:22 +0200

    de component gemaakt

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+// { items: [], heading: "string" }
+interface Props {
+	items: string[];
+	heading: string;
+}
+
+
+
+function ListGroup(props: Props) {
+	let items = ["jan", "kees", "lonk", "cronk", "mohammed"];
+	const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  	return (
+	<>
+		<h1>List Group</h1>
+		{ items.length === 0 && <p>No items found</p> }
+		<ul className="list-group">
+			{items.map((item, index) => (
+			<li 
+			className={selectedIndex === index ? "list-group-item active" : "list-group-item"} 
+			key={item} 
+			onClick={() => setSelectedIndex(index)}
+			>{item}</li>
+		))
+			}
+		</ul>
+	</>
+  );
+}
+
+export default ListGroup;
